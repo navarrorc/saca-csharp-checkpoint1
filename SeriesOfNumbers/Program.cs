05b043d362038c4d0ac7afb8b36db085a762710b
Roberto C Navarro <rnavarro70@gmail.com>
2018-02-14 11:19:02 -0600

    implement SeriesOfNumbers exercise

--- SeriesOfNumbers/Program.cs
+++ SeriesOfNumbers/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SeriesOfNumbers
 {
@@ -10,6 +7,29 @@ namespace SeriesOfNumbers
     {
         static void Main(string[] args)
         {
+            string input = "";
+            int[] numbersArray = null;
+
+            Console.WriteLine("Enter a series of number separated by a comma:");
+            input = Console.ReadLine();
+
+            try
+            {
+                // see: https://www.dotnetperls.com/exception
+                // This try catch helps in handling exceptions, we can display messages in the catch block when the user forgets to provide the correct input
+                numbersArray = Array.ConvertAll(input.Split(','), int.Parse); // see: https://www.dotnetperls.com/array-convertall for `Array.ConvertAll`
+                int maxValue = numbersArray.Max();
+                Console.WriteLine("The max value is: " + maxValue);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                Console.WriteLine("***Did you forget to only include numbers?***");
+            }
+
+            Console.ReadLine();
+
         }
     }
 }
