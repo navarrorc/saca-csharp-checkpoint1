59e9d15e530583760c8dbad00f655bd5ef42092f
Roberto C Navarro <rnavarro70@gmail.com>
2018-02-13 20:48:03 -0600

    Initial Commit

---
+++ DivisibleBy3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DivisibleBy3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1 to 100, find divisible by 3 with 0 remainder
+
+            //int[] numbers = Enumerable.Range(1, 100).ToArray();
+
+            int count = 0;
+
+            for (int i = 1; i < 100; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine("Total Count: " + count);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ SumOfNumbers/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumOfNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string input = Console.ReadLine();
+            //int number = 0;
+
+            string input = "";
+            int number = 0;
+            int total = 0;
+
+            while (input != "ok")
+            {
+                Console.WriteLine("Please a number or type 'ok' to quit");
+                input = Console.ReadLine();
+
+                int.TryParse(input, out number);
+
+                if (number > 0)
+                {
+                    //total = total + number;
+                    total += number;
+                    number = 0;
+                }
+
+            }
+
+            Console.WriteLine("Total: " + total);
+            Console.ReadLine();
+
+        }
+    }
+}
